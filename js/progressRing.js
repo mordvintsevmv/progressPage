f73a627bd29e475ec391c4654cd1e388b1175598
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-09-02 02:05:50 +0300

    Moving class progressRing to a separate file

---
+++ js/progressRing.js
@@ -0,0 +1,58 @@
+export default class progressRing{
+
+    constructor(class_name) {
+        this.circle = document.querySelector(`.${class_name}`);
+        this.radius = this.circle.r.baseVal.value;
+        this.circle_length = 2 * Math.PI * this.radius;
+        this.progress = 0;
+    }
+
+    print_radius(){
+        console.log(this.radius);
+    }
+
+    print_circle_length(){
+        console.log(this.circle_length);
+    }
+
+    print_progress(){
+        console.log(this.progress);
+    }
+
+    set_progress(progress){
+        this.progress = progress;
+        if (this.progress === 0){
+            this.circle.style.opacity = "0";
+        } else{
+            const fill = (this.progress/100) * this.circle_length;
+            const empty = this.circle_length - fill;
+
+            this.circle.style.strokeDasharray = `${fill} ${empty}`;
+            this.circle.style.opacity = "1";
+        }
+
+    }
+
+    set_rotate_duration(duration){
+        this.circle.style.animationDuration = `${duration}s`;
+    }
+
+    animation_turnon(){
+        this.circle.style.animationPlayState = `running`;
+    }
+
+    animation_turnoff(){
+        this.circle.style.animationPlayState = `paused`;
+    }
+
+    hide(){
+        this.circle.style.opacity = "0";
+    }
+
+    show(){
+        if (this.progress !== 0) {
+            this.circle.style.opacity = "1";
+        }
+    }
+
+}
--- js/ring_actions.js
+++ js/ring_actions.js
@@ -1,63 +1,7 @@
-class progressRing{
+import progressRing from './progressRing.js';
 
-    constructor(class_name) {
-        this.circle = document.querySelector(`.${class_name}`);
-        this.radius = this.circle.r.baseVal.value;
-        this.circle_length = 2 * Math.PI * this.radius;
-        this.progress = 0;
-    }
-
-    print_radius(){
-        console.log(this.radius);
-    }
-
-    print_circle_length(){
-        console.log(this.circle_length);
-    }
-
-    print_progress(){
-        console.log(this.progress);
-    }
-
-    set_progress(progress){
-        this.progress = progress;
-        if (this.progress === 0){
-            this.circle.style.opacity = "0";
-        } else{
-            const fill = (this.progress/100) * this.circle_length;
-            const empty = this.circle_length - fill;
-
-            this.circle.style.strokeDasharray = `${fill} ${empty}`;
-            this.circle.style.opacity = "1";
-        }
-
-    }
-
-    set_rotate_duration(duration){
-        this.circle.style.animationDuration = `${duration}s`;
-    }
-
-    animation_turnon(){
-        this.circle.style.animationPlayState = `running`;
-    }
-
-    animation_turnoff(){
-        this.circle.style.animationPlayState = `paused`;
-    }
-
-    hide(){
-        this.circle.style.opacity = "0";
-    }
-
-    show(){
-        if (this.progress !== 0) {
-            this.circle.style.opacity = "1";
-        }
-    }
-
-}
-
-let circle = new progressRing("progress_ring_circle");
+const circle = new progressRing("progress_ring_circle");
+console.log(circle)
 
 const percent_input = document.querySelector('.value_block_percent');
 circle.set_progress(percent_input.value);
