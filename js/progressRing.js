10a77f336ab63785fcd40d392b76c8db1f802de9
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-09-03 08:11:28 +0300

    Added comments

--- js/progressRing.js
+++ js/progressRing.js
@@ -1,5 +1,14 @@
+/*
+    Класс progressRing позволяет создать кольцо прогресса.
+ */
+
 export default class progressRing{
 
+    /*
+        Конструктор принимает id тега <circle> и получает объект со страницы.
+        Также в конструкторе вычисляется радиус и длина окружности.
+        Задается начальный прогресс и цвет шкалы.
+     */
     constructor(id) {
         this.circle = document.querySelector(`#${id}`);
         this.radius = this.circle.r.baseVal.value;
@@ -8,6 +17,10 @@ export default class progressRing{
         this.circle.style.stroke = "#0B5DFF";
     }
 
+
+    /*
+        Методы, позволяющие получить основные данные объекта класса progressRing.
+     */
     print_radius(){
         console.log(this.radius);
     }
@@ -24,9 +37,16 @@ export default class progressRing{
         console.log(this.circle.style.stroke);
     }
 
+    /*
+        Метод для редактирования значения прогресса и изменения его отображения.
+     */
     set_progress(progress){
         this.progress = progress;
 
+
+        /*
+           Если прогресс находится в пределах 1-100 и более, задаётся необходимая длина отрезков.
+         */
         if ((progress < 100) && (progress > 0)){
 
             const fill = (this.progress/100) * this.circle_length;
@@ -40,7 +60,13 @@ export default class progressRing{
             this.circle.style.strokeDasharray = `${this.circle_length} 0`;
             this.circle.style.opacity = "1";
 
-        } else if (progress <= 0){
+        }
+
+        /*
+           Если прогресс ниже нуля, то непрозрачность кольца становится равной нулю.
+           Такой подход позволяет избежать маленькую полоску прогресса (равную 1%), которая возникает из-за деления на ноль.
+         */
+        else if (progress <= 0){
 
             this.circle.style.opacity = "0";
 
@@ -48,30 +74,50 @@ export default class progressRing{
 
     }
 
+    /*
+       Метод, позволяющий установить период анимации (в секундах).
+       Выполнена проверка на правильность ввода числа.
+     */
     set_rotate_duration(duration){
         if ((/\d/) && (duration > 0)) {
             this.circle.style.animationDuration = `${duration}s`;
         }
     }
 
+    /*
+       Метод, позволяющий установить цвет шкалы прогресса в формате HEX (#CD5C5C).
+       Выполнена проверка на ввод корректного формата.
+     */
     set_color(color){
         if ((!(/[^\da-fA-F]/).test(color)) && (color.length === 6)) {
             this.circle.style.stroke = `#${color}`;
         }
     }
 
+    /*
+       Метод, включающий анимацию.
+     */
     animation_turnon(){
         this.circle.style.animationPlayState = `running`;
     }
 
+    /*
+       Метод, выключающий анимацию.
+     */
     animation_turnoff(){
         this.circle.style.animationPlayState = `paused`;
     }
 
+    /*
+       Метод, скрывающий кольцо прогресса.
+     */
     hide(){
         this.circle.style.opacity = "0";
     }
 
+    /*
+       Метод, возвращающий видимость кольца прогресса.
+     */
     show(){
         if (this.progress > 0) {
             this.circle.style.opacity = "1";
--- js/ring_actions.js
+++ js/ring_actions.js
@@ -1,21 +1,39 @@
 import progressRing from './progressRing.js';
 
+/*
+   Создаём объекта класса для нужно кольца.
+ */
 const circle = new progressRing("progress_ring_circle_1");
 
+
+
+/*
+   Получаем input со значением прогресса.
+   Задаём начальное значение прогресса кольцу.
+   Добавляем обработчик событий на изменение значения для моментольного отображения нового прогресса.
+ */
 const percent_input = document.querySelector('.value_block_percent');
+
 circle.set_progress(percent_input.value);
+
 percent_input.addEventListener('keyup', function (){
     circle.set_progress(percent_input.value);
 })
 
+
+/*
+   Получаем input (checkbox) с состоянием анимации.
+   Включаем анимацю сразу, если checkbox отмечен.
+   Добавляем обработчик событий на изменение состояния checkbox.
+ */
 const animation_button = document.querySelector('.animation_button');
 
-if (animation_button.checked === true){
+if (animation_button.checked){
     circle.animation_turnon();
 }
 
 animation_button.addEventListener('click', function (){
-    if (animation_button.checked === true){
+    if (animation_button.checked){
         circle.animation_turnon();
     } else {
         circle.animation_turnoff();
@@ -23,15 +41,25 @@ animation_button.addEventListener('click', function (){
 })
 
 
+/*
+   Получаем input (checkbox) с состоянием отображения.
+   Выключаем отображение сразу, если checkbox отмечен.
+   Добавляем обработчик событий на изменение состояния checkbox.
+
+   Вместе с отключением отображения также:
+    Перестают работать остальные кнопки.
+    Видимость кнопок снижается.
+    Анимация кольца останавливается.
+ */
 const hide_button = document.querySelector('.hide_button');
 
-if (hide_button.checked === true){
+if (hide_button.checked){
     animation_button.disabled = true;
     document.querySelector(".progress_ring").hidden = true;
 }
 
 hide_button.addEventListener('click', function (){
-    if (hide_button.checked === true){
+    if (hide_button.checked){
         animation_button.disabled = true;
         percent_input.disabled = true;
 
